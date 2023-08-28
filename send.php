3a58190bc8424f71878880087344c84faadb3324
yevheniivintoniak290 <143408841+yevheniivintoniak290@users.noreply.github.com>
2023-08-28 15:18:05 +0300

    Create send.php

---
+++ send.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Remove Your Data from the Game</title>
+  </head>
+  <body>
+    <p>To remove your data from the game, please email our support team at <a href="mailto:yevheniivintoniak290@gmail.com">yevheniivintoniak290@gmail.com</a>. We will definitely review your request within 24 hours.</p>
+  </body>
+</html>
+
